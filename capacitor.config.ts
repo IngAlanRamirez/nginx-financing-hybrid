b30bf14a8779f0d956185900d9b4a783d50f0e66
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-07-19 15:12:54 -0600

    Enhance Capacitor configuration with HTTP plugin support and update main.ts to include HttpClient provider

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'nginx-financing-hybrid',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
